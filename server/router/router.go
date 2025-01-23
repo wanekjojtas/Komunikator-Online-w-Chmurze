3529615f3aac5516f93b5ea3c7cd3ed2fdac29fa
wanekjojtas <wanek.jojtas@gmail.com>
2025-01-23 22:10:46 +0100

    updates to CORS policy

--- server/router/router.go
+++ server/router/router.go
@@ -18,7 +18,7 @@ var r *gin.Engine
 func getAllowedOrigins() []string {
 	// Allow different origins for development and production
 	if gin.Mode() == gin.ReleaseMode {
-		return []string{"https://golang-nextjs-chat-app-fe-f621f6c2c0af.herokuapp.com"}
+		return []string{"https://golang-nextjs-chat-app-fe-f621f6c2c0af.herokuapp.com/"}
 	}
 	return []string{"http://localhost:3000"}
 }
@@ -26,7 +26,7 @@ func getAllowedOrigins() []string {
 func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 	r = gin.Default()
 
-	// CORS Configuration
+	// Apply CORS middleware globally
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     getAllowedOrigins(),
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -36,11 +36,13 @@ func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	// Log all incoming requests
+	// Log all incoming requests except OPTIONS
 	r.Use(func(c *gin.Context) {
-		log.Printf("Request: %s %s", c.Request.Method, c.Request.URL.Path)
-		if strings.HasPrefix(c.Request.URL.Path, "/ws/") {
-			log.Printf("WebSocket Request Parameters: %v", c.Request.URL.Query())
+		if c.Request.Method != "OPTIONS" {
+			log.Printf("Request: %s %s", c.Request.Method, c.Request.URL.Path)
+			if strings.HasPrefix(c.Request.URL.Path, "/ws/") {
+				log.Printf("WebSocket Request Parameters: %v", c.Request.URL.Query())
+			}
 		}
 		c.Next()
 	})
@@ -72,29 +74,6 @@ func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 		authRoutes.GET("/getChatMessages/:chatID", wsHandler.GetChatMessages)
 	}
 
-	// OPTIONS Handler for Preflight Requests
-	r.OPTIONS("/*path", func(c *gin.Context) {
-		origin := c.GetHeader("Origin")
-		if contains(getAllowedOrigins(), origin) {
-			c.Header("Access-Control-Allow-Origin", origin)
-		}
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Connection", "Upgrade")
-		c.Header("Upgrade", "websocket")
-		c.AbortWithStatus(204)
-	})
-}
-
-// Utility to check if a string is in a slice
-func contains(slice []string, item string) bool {
-	for _, s := range slice {
-		if s == item {
-			return true
-		}
-	}
-	return false
 }
 
 func Start(addr string) error {
